931592116e6b81cab32a665cdea1039245859e47
omran96 <40178008+omran96@users.noreply.github.com>
2021-10-23 16:02:44 +0200

    Delete property.py

--- property.py
+++
@@ -1,7 +0,0 @@
-import scrapy
-from itemloaders.processors import Join
-
-
-class Property(scrapy.Item):
-    title = scrapy.Field(output_processor=Join())
-    price = scrapy.Field(output_processor=Join())
